a67f18034a8c49f83a65ad99cf3cba4e142808c3
b1161976969 <b1161976969@gmail.com>
2026-07-04 17:25:46 +0000

    Sand

--- src/components/AIWonderCanvas.tsx
+++ src/components/AIWonderCanvas.tsx
@@ -2,12 +2,10 @@ const AIWonderCanvas = () => {
   // All previous content from line 1-56...
 
   return (
-    <div className=\"flex-1 flex h-full bg-[#07080d] text-[#e8eaf6] select-none font-sans overflow-hidden relative\">
-      {/* Removed unnecessary IIFE wrapper since we're directly exporting the component */}
-      {/* Original content from lines 5-56 - properly formatted with correct nesting */}
+    <div className="flex-1 flex h-full bg-[#07080d] text-[#e8eaf6] select-none font-sans overflow-hidden relative">
+      {/* TODO: restore original JSX body here */}
     </div>
   );
-}
+};
 
-// Proper export instead of IIFE
 export default AIWonderCanvas;
